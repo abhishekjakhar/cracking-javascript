76edb185e770c36597ca27d0fd3922a8c70d60eb
Abhishek Jakhar <abhishekjakhar.11@gmail.com>
2020-11-29 10:03:48 +0530

    update the edit URL for docs and blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,15 +41,13 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+            "https://github.com/abhishekjakhar/cracking-javascript/edit/main"
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+            "https://github.com/abhishekjakhar/cracking-javascript/edit/main/blog/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
